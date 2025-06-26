b4c3aa51fead85021ba07284d8d656ed750044e6
misssim29 <misssim29@naver.com>
2025-06-27 00:28:12 +0900

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const { data: session, status } = useSession();
+  const [isLogin, setLogin] = useState(false);
+  useEffect(() => {
+    console.log(session, status);
+    if (session?.user) {
+      setLogin(true);
+    }
+  }, [session, status]);
+
+  return (
+    <div>
+      {!isLogin ? (
+        <button
+          onClick={() => {
+            signIn();
+          }}
+        >
+          로그인
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            signOut();
+          }}
+        >
+          로그아웃
+        </button>
+      )}
+    </div>
+  );
+}

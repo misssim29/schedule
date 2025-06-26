92e6aabb58fdb11148c8cc0b2f0d951be4448c53
misssim29 <misssim29@naver.com>
2025-06-27 00:35:34 +0900

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,7 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_OAUTH_ID!,
-      clientSecret: process.env.GOOGLE_OAUTH_SECRET!,
-    }),
-  ],
-};
+import { authOptions } from "@/app/lib/auth";
 
 const handler = NextAuth(authOptions);
+
 export const GET = handler;
 export const POST = handler;

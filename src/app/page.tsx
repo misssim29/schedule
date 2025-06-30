09e0efad0b81e0114b113439746313fbae6c9cfb
Levup <65381839+misssim29@users.noreply.github.com>
2025-06-30 16:34:14 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,16 @@
-"use client";
-
-import { signIn, signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import Calendar from '@/components/Calendar';
+import HolidayForm from '@/components/HolidayForm';
+import UserState from '@/components/UserState';
+import styles from './home.module.scss';
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  const [isLogin, setLogin] = useState(false);
-  useEffect(() => {
-    console.log(session, status);
-    if (session?.user) {
-      setLogin(true);
-    }
-  }, [session, status]);
-
-  return (
-    <div>
-      구글로그인
-      {!isLogin ? (
-        <button
-          onClick={() => {
-            signIn();
-          }}
-        >
-          로그인
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            signOut();
-          }}
-        >
-          로그아웃
-        </button>
-      )}
-    </div>
-  );
+    return (
+        <div className={styles.wrap}>
+            <Calendar />
+            <div>
+                <UserState />
+                <HolidayForm />
+            </div>
+        </div>
+    );
 }
